9859642738ae17c87847143b038abb8977e1f197
fakhrideen <fakhri.deen@gmail.com>
2015-01-20 00:08:08 -0600

    delete test file

--- app/src/main/java/com/example/fakhidean/kaigiapp/main_page.java
+++ app/src/main/java/com/example/fakhidean/kaigiapp/main_page.java
@@ -7,7 +7,6 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,11 +23,13 @@ public class main_page extends FragmentActivity implements LocationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
         map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this); //You can also use LocationManager.GPS_PROVIDER and LocationManager.PASSIVE_PROVIDER
+
         map.setMyLocationEnabled(true);
         map.addMarker(new MarkerOptions()
                 .position(new LatLng(29.940348,-90.120728))
